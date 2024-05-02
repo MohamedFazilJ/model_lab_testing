3294d6f1796406c8432e0db01c7a9491061e384a
J Mohamed Fazil <124855920+MohamedFazilJ@users.noreply.github.com>
2024-05-02 11:32:52 +0530

    Add files via upload

---
+++ modell/groww/gameblaze/src/main/java/com/example/gameblaze/GameblazeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameblaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameblazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameblazeApplication.class, args);
+	}
+
+}
